0b5c5dacadb98399f3c16f15804c344dc4a79a22
Andrew Rubin <arubin78@gmail.com>
2022-06-08 15:12:04 -0700

    docs(app): add project-specific image example

--- src/components/app.js
+++ src/components/app.js
@@ -1,3 +1,10 @@
+import sampleImage from '../assets/2x1_PMCSGalleryOpeningStory_v01-01.jpg';
+
 export function App() {
-  return <h1>Hello world!</h1>;
+  return (
+    <>
+      <h1>Hello world!</h1>
+      {/* <img src={sampleImage} alt="" /> */}
+    </>
+  );
 }
