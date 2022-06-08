db48691a2d53c24f738011c1345ecdb0203d65c1
Andrew Rubin <arubin78@gmail.com>
2022-06-08 15:39:26 -0700

    feat: add data file

---
+++ src/data/items.js
@@ -0,0 +1,65 @@
+import img1 from "../assets/2x1_PMCSGalleryOpeningStory_v01-01.jpg";
+import img2 from "../assets/2x1_PMCSGalleryOpeningStory_v01-02.jpg";
+import img3 from "../assets/2x1_PMCSGalleryOpeningStory_v01-03.jpg";
+import img4 from "../assets/2x1_PMCSGalleryOpeningStory_v01-04.jpg";
+import img5 from "../assets/2x1_PMCSGalleryOpeningStory_v01-05.jpg";
+import img6 from "../assets/2x1_PMCSGalleryOpeningStory_v01-06.jpg";
+import img7 from "../assets/2x1_PMCSGalleryOpeningStory_v01-07.jpg";
+import img8 from "../assets/2x1_PMCSGalleryOpeningStory_v01-08.jpg";
+import img9 from "../assets/2x1_PMCSGalleryOpeningStory_v01-09.jpg";
+import img10 from "../assets/2x1_PMCSGalleryOpeningStory_v01-10.jpg";
+import img11 from "../assets/2x1_PMCSGalleryOpeningStory_v01-11.jpg";
+import img12 from "../assets/2x1_PMCSGalleryOpeningStory_v01-12.jpg";
+
+export const items = [
+  {
+    caption: "Ea consequat eiusmod irure pariatur cupidatat esse sint magna.",
+    image: img1,
+  },
+  {
+    caption: "Non elit amet quis ad est sint est minim.",
+    image: img2,
+  },
+  {
+    caption:
+      "Proident excepteur elit Lorem culpa dolor reprehenderit cupidatat.",
+    image: img3,
+  },
+  {
+    caption: "Commodo deserunt ut cupidatat nisi qui qui nostrud.",
+    image: img4,
+  },
+  {
+    caption: "Exercitation pariatur reprehenderit adipisicing dolor cupidatat.",
+    image: img5,
+  },
+  {
+    caption: "Velit nulla incididunt dolor sit.",
+    image: img6,
+  },
+  {
+    caption: "Eiusmod reprehenderit esse est Lorem.",
+    image: img7,
+  },
+  {
+    caption: "Ut ullamco proident eu id esse non eu nulla.",
+    image: img8,
+  },
+  {
+    caption:
+      "Ea voluptate dolore anim commodo deserunt ex exercitation laboris.",
+    image: img9,
+  },
+  {
+    caption: "Aliqua labore in mollit proident.",
+    image: img10,
+  },
+  {
+    caption: "Velit fugiat deserunt fugiat velit consequat ea occaecat fugiat.",
+    image: img11,
+  },
+  {
+    caption: "Sit exercitation qui laboris deserunt.",
+    image: img12,
+  },
+];
