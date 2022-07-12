eada64822d6621399d5c7fb6c981197c762d1fca
Andrew Rubin <arubin78@gmail.com>
2022-07-12 09:29:20 -0700

    build: add parcel SVG transformer

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,4 @@
-import sampleImage from '../assets/2x1_PMCSGalleryOpeningStory_v01-01.jpg';
+// import sampleImage from "../assets/2x1_PMCSGalleryOpeningStory_v01-01.jpg";
 
 export function App() {
   return (
